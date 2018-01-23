52e3172abed2e70af69e1fc3f62ed584968d6aae
zhangxu261 <zhangxu261@gmail.com>
2018-01-23 15:31:36 +0800

    fix

---
+++ src/main/java/io/github/okooo/OkoooApplication.java
@@ -0,0 +1,17 @@
+package io.github.okooo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@MapperScan("io.github.okooo.mapper")
+@EnableScheduling
+@SpringBootApplication
+public class OkoooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OkoooApplication.class, args);
+    }
+
+}
