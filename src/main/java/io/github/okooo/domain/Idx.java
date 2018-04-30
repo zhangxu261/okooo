fc64550684056162b8d08163286639d14bc47cfd
张旭 <zhangxu@pseer.com>
2018-04-30 10:26:48 +0800

    fix

--- src/main/java/io/github/okooo/domain/Idx.java
+++ src/main/java/io/github/okooo/domain/Idx.java
@@ -2,6 +2,7 @@ package io.github.okooo.domain;
 
 import com.baomidou.mybatisplus.annotations.TableId;
 import com.baomidou.mybatisplus.annotations.TableName;
+import com.baomidou.mybatisplus.enums.IdType;
 
 import java.util.Date;
 
